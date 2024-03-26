08570e7f3e4116abe58da661e2e29b827b1929a6
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-03-26 08:18:32 -0300

    first commit

---
+++ Atividade.js
@@ -0,0 +1,20 @@
+let numeros = [1, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let par = [];
+let impar = [];
+let i = 0;
+
+while (i < numeros.length) {
+    if (numeros[i] % 2 === 0) {
+        par.push(numeros[i]);
+    } else {
+        impar.push(numeros[i]);
+    }
+    i++;
+}
+par.sort((a, b) => a - b);
+impar.sort((a, b) => a - b);
+
+par.reverse();
+
+console.log("Números pares:", par);
+console.log("Números ímpares:", impar);i"# Atividades" 
